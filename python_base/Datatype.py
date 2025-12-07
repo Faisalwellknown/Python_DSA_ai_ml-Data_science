7046d3adf569432be1eeda37194cef70ec346d31
Faisal Shaikh <faisalshaikhikjf@gmail.com>
2025-12-08 00:58:02 +0530

    added Datatype .py file

---
+++ python_base/Datatype.py
@@ -0,0 +1,16 @@
+X ="Faisal Shaikh" #String
+
+z=20 # Integer
+c=10.4 # Float
+g = 1j  #Complex
+x = ["Faisal","Amar","abdul"] #list
+x = ("Faisal","Amar","abdul") #tuple
+x = range(8) #range
+x = {"name " : "Faisal", "age" : 23}#dictionary
+x = {"apple","banana","cherry"} #set
+x = frozenset ({"Apple","banana", "cherry"}) #frozenset
+x = True #boolean
+x= b"Hello" #bytes
+x = bytearray(5) #bytearray
+x= memoryview(bytes(5)) #memoryview
+x= None #none type
