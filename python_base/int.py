8f0ff079d9da3cce8af63094b0227e65cb8edcc5
Faisal Shaikh <faisalshaikhikjf@gmail.com>
2025-12-11 01:18:14 +0530

    updated int.py

--- python_base/int.py
+++ python_base/int.py
@@ -1,1 +1,20 @@
-hi
+x = 1
+y = 2.8
+z = 1j
+
+a = float(x)
+
+b = int (y)
+
+c = complex(x)
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+import random
+print(random.randrange(1,10))
