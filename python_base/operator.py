5149bfdd1a607ab8872d375eaa9fd2466c13ba2a
Faisal Shaikh <faisalshaikhikjf@gmail.com>
2025-12-20 07:00:26 +0530

    added Operators.py

---
+++ python_base/operator.py
@@ -0,0 +1,13 @@
+"""Python divides the operators in the following groups:
+
+Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+Bitwise operators                       """
+
+
+
+print(10+2)
