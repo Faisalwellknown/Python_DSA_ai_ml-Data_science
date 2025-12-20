e05fd203f8cc992df25f6df4b2a196b191c72ca1
Faisal Shaikh <faisalshaikhikjf@gmail.com>
2025-12-21 03:26:43 +0530

    added assingment.py

---
+++ python_base/assingment.py
@@ -0,0 +1,19 @@
+""" Assignment Operators
+Assignment operators are used to assign values to variables:
+
+Operator	Example	Same As	Try it
+=	x = 5	x = 5	
++=	x += 3	x = x + 3	
+-=	x -= 3	x = x - 3	
+*=	x *= 3	x = x * 3	
+/=	x /= 3	x = x / 3	
+%=	x %= 3	x = x % 3	
+//=	x //= 3	x = x // 3	
+**=	x **= 3	x = x ** 3	
+&=	x &= 3	x = x & 3	
+|=	x |= 3	x = x | 3	
+^=	x ^= 3	x = x ^ 3	
+>>=	x >>= 3	x = x >> 3	
+<<=	x <<= 3	x = x << 3	
+:=	print(x := 3)	x = 3
+print(x)    """
