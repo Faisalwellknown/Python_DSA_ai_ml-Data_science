5a00ad7017be077cc428a5da2c228277143904ad
Faisal Shaikh <faisalshaikhikjf@gmail.com>
2025-12-21 03:51:38 +0530

    updated assingment.py

--- python_base/assingment.py
+++ python_base/assingment.py
@@ -17,3 +17,12 @@ Operator	Example	Same As	Try it
 <<=	x <<= 3	x = x << 3	
 :=	print(x := 3)	x = 3
 print(x)    """
+
+
+numbers = [1, 2, 3, 4, 5]
+count = len(numbers)
+if count > 3:
+    print(f"List has {count} elements")
+
+if (count := len(numbers)) > 3:
+    print(f"List has {count} elements")
