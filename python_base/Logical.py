bbc13add5e7983c9e8c30f9f56f5785a4fedfc9a
Faisal Shaikh <faisalshaikhikjf@gmail.com>
2025-12-26 08:26:05 +0530

    Updated 1.0  logical.py

--- python_base/Logical.py
+++ python_base/Logical.py
@@ -4,4 +4,5 @@ or	Returns True if one of the statements is true	x < 5 or x < 4
 not	Reverse the result, returns False if the result is true	not(x < 5 and x < 10)	
  """
  
- 
+x = 5
+print(x> 0 and x<10)
